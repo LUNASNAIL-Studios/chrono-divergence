ec8cad2479b07b8bafd3cf5d75448386a0f29f9a
Steffen Kahl <stevenpaw209@gmail.com>
2022-12-21 14:15:06 +0100

    [ADDED] Firework Scene

---
+++ Chrono Divergence/Assets/_GameAssets/05_Scripts/Openables/Firework.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ChronoDivergence
+{
+    public class Firework : MonoBehaviour, IOpenable
+    {
+        [SerializeField] private UnityEvent OnStart;
+        [SerializeField] private GameObject[] neededIActivators;
+            
+        public bool IsOpened()
+        {
+            return false;
+        }
+
+        public bool IsActivatable()
+        {
+            return true;
+        }
+
+        public void SetActivatable(bool activatable)
+        {
+            //Do nothing
+        }
+
+        private void Update()
+        {
+            //TODO: Implement with events!
+            CheckActivators();
+        }
+
+        private void CheckActivators()
+        {
+            bool allActive = true;
+            foreach (GameObject activatable in neededIActivators)
+            {
+                IActivatable activatableThing = activatable.GetInterfaces<IActivatable>()[0];
+                if (!activatableThing.IsActivated())
+                {
+                    allActive = false;
+                }
+            }
+
+            if (allActive)
+            {
+                OnStart.Invoke();
+            }
+            else
+            {
+            }
+        }
+    }
+}
